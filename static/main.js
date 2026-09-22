017aacc14a68b85004b211ecb364e42cb055ec3a
Firas12123 <firascosta35@gmail.com>
2026-09-22 18:06:05 +0100

    password toggle with JS

--- static/main.js
+++ static/main.js
@@ -1,25 +1,53 @@
-let myButton = document.querySelector(".sign-in");
-let login_form = document.querySelector(".login-form");
-let overlay = document.querySelector("#overlay")
-let cancel = document.querySelector(".cancel-button")
+const myButton = document.querySelector(".sign-in");
+const login_form = document.querySelector(".login-form");
+const overlay = document.querySelector("#overlay");
+const cancel = document.querySelector(".cancel-button");
+const pass_button = document.querySelector("#toggle-password");
+const eye_open = document.querySelector("#eye-open");
+const eye_closed = document.querySelector("#eye-closed");
+const show_password = document.getElementById("password-form")
+let seePassword = false
 
 
+// opening the login form and bluring the background if called
 function loggedIn(){
     myButton.textContent = "My Profile";
     login_form.style.display = "inline-block";
-    login_form.classList.add('active');
-    overlay.classList.add('active');
+    login_form.classList.add("active");
+    overlay.classList.add("active");
 }
 
-function closeLogin(){
-    login_form.classList.remove('active');
-    overlay.classList.remove('active');
+function passwordEye() {
+    seePassword = !seePassword
+    if (seePassword === true){
+        eye_open.classList.add("active")
+        eye_closed.classList.add("active")
+        show_password.type = "text"
+
+    }
+    else{
+        eye_open.classList.remove("active")
+        eye_closed.classList.remove("active")
+        show_password.type ="password"
+    }
 }
+
+
+
+// close the login page if called
+function closeLogin(){
+    login_form.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+// calling the functions
 myButton.addEventListener("click", loggedIn)
 if (myButton.textContent !== "My Profile"){
     myButton.addEventListener("click", loggedIn)
 } else{
     // do nothing yet on click
 }
+
+pass_button.addEventListener("click", passwordEye)
 overlay.addEventListener("click", closeLogin)
 cancel.addEventListener("click", closeLogin)
